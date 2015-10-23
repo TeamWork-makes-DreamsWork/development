312e2b74499e94c8395c97ac438cd30677f437f5
Da-Me <damekim1206@gmail.com>
2015-10-23 16:26:10 -0400

    add vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,7 @@
 class VotesController < ApplicationController
+  def index
+    @votes = Vote.all
+  end
   def create
     vote = Vote.new(vote: params[:vote], member_id: params[:member_id], vote_for_id: params[:vote_for_id], vote_for_type: params[:vote_for_type])
    if vote.save
---
+++ app/views/votes/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @votes, :member_id, :vote, :vote_for_id, :vote_for_type
