116a19df9afc5faea4e01906f9880281e611ca47
Da-Me <damekim1206@gmail.com>
2015-10-25 18:53:10 -0400

    add bcrypt

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,10 +2,9 @@ class Member < ActiveRecord::Base
   has_many :votes
   has_many :questions
   has_many :answers
-
   # validates :email, uniqueness: true
   # validates :name, :email, :password, presence: true
-  # has_secure_password
+  has_secure_password
   before_create :assign_token
 
   def assign_token
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ActiveRecord::Base
   has_many :answers
   has_many :votes, as: :votes_for
   belongs_to :member
-  # before_create :check_token
+  before_create :check_token
 
   def member_name
     member.name
