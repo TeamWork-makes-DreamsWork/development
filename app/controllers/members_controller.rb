4a5c3f6eeb0a5ea0f5fd6e2a988fd3970fe92369
Da-Me <damekim1206@gmail.com>
2015-10-25 17:50:57 -0400

    add contoller and model for members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -8,29 +8,40 @@ class MembersController < ApplicationController
   def create
     m = Member.new(user_params)
      if m.save
-       render person
-     elsif m.errors
-       render json: "Bad Request"
+       render json: m
      else
-       render json: "Could not save member"
+       render json: m.errors
      end
   end
 
   def show
-
+    render json: Member.find(params[:id])d
   end
 
   def new
+    @member = Member.new
+  end
+
+  def update
+    if @member.update(member_params)
+      redirect_to @member, notice: 'Member was successfully updated.'
+    else
+      render json: "Update Unsuccessful"
+    end
   end
 
   def destroy
     @member.destroy
-    render json: "Member deleted"
+    render json: "Member destroyed"
   end
 private
 
+def set_member
+    @member = Member.find(params[:id])
+  end
+
   def member_params
-    params.permit(:name, :email, :password, :token)
+    params.require(:member).permit(:name, :email, :password, :token)
   end
 
 
