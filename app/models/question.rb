20353e6f59470d48c4824fa8146d446ecb9cc97a
Da-Me <damekim1206@gmail.com>
2015-10-25 13:24:57 -0400

    add member_name

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,9 @@ class Question < ActiveRecord::Base
   has_many :answers
   has_many :votes, as: :votes_for
   belongs_to :member
-  
+
   #before_create :check_token
+  def member_name
+    member.name
+  end
 end
